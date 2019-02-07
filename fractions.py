03e07c05b064c7fddbfcb9a2a34fb784e1704659
richul <richuln6@gmail.com>
2019-02-07 10:56:55 +0530

    update.

--- fractions.py
+++ fractions.py
@@ -12,13 +12,36 @@ print(str(tr[0])+"/"+str(tr[1]))
 
 
 
-
 class fractionclass:
-    int num,den
-    def input():
-        num=input("enter the numerator")
-        den=input("enter the denomenator")
+    def __init__(self,a,b):
+        self.numerator=a
+        self.denomenator=b
+    def add(f1,f2):
+        num=(f1.numerator*f2.denomenator)+(f1.denomenator*f2.numerator)
+        den=f1.denomenator*f2.denomenator
         return (num,den)
+f1=fractionclass(2,4)
+f2=fractionclass(1,2)
+t=fractionclass.add(f1,f2)
+print(str(t[0])+"/"+str(t[1]))
 
-fractionclass obj=new 
-        
+
+def fractiondictionary(t1,t2):
+    d={}
+    d["Numerator"]=(t1[0]*t2[1])+(t2[0]*t1[1])
+    d["denomenator"]=t1[1]*t2[1]
+    return d
+
+D=fractiondictionary((2,4),(1,2))
+print(str(D["Numerator"])+"/"+str(D["denomenator"]))
+
+def fractionslist(t1,t2):
+    num=(t1[0]*t2[1])+(t1[1]*t2[0])
+    den=(t1[1]*t2[1])
+    res=[num,den]
+    return res
+
+d=fractionslist((1,1),(4,2))
+print(str(d[0])+"/"+str(d[1]))
+
+
