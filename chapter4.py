f3264b2fcbe392cc51f0800470a9fe7701fce172
richul <richuln6@gmail.com>
2019-02-19 00:04:43 +0530

    upgrade

---
+++ chapter4.py
@@ -0,0 +1,16 @@
+#1
+import random
+x=random.randint(5,10)
+print(x)
+
+
+#2
+def print_lyrics():
+    print("So baby pull me closer")
+    print("on the backseat of you're rover")
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+repeat_lyrics()
+
