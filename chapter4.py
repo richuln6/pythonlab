10c8ac8028adfc5dad4b51185825ffd86c349d2a
richul <richuln6@gmail.com>
2019-02-19 20:56:17 +0530

    upgarde

--- chapter4.py
+++ chapter4.py
@@ -14,3 +14,47 @@ def repeat_lyrics():
 
 repeat_lyrics()
 
+
+#6
+
+def computepay(hours,rate):
+    try:
+        if hours<=40:
+            pay=hours*(rate)
+        else:
+            pay=(40*(rate))+((hours-40)*1.5*rate)
+        return pay
+    except:
+        return("Enter Numeric Input")
+
+hours=float(input("Enter Hours"))
+rate=float(input("enter rate"))
+s=computepay(hours,rate)
+print(s)
+
+
+#7
+def grade_score(grade):
+    try:
+        if grade>0.0 and grade<1.0:
+            if grade>=0.9:
+                return("A")
+            elif grade>=0.8:
+                return "B"
+            elif grade>=0.7:
+                return "C"
+            elif grade>=0.6:
+                return "D"
+            else :
+                return "F"
+        else:
+            return "Score Out Of Range"
+    except:
+        return "Bad Score"
+
+grade=input("Enter Score:")
+try:
+    g=grade_score(float(grade))
+except:
+    g="Bad Score"
+print(g)
--- fibonacci.py
+++ fibonacci.py
@@ -12,4 +12,18 @@ fib(10)
 
 
 
+def fib():
+    x,y=0,1
+    print(x)
+    print(y)
+    while(True):
+        yield (x+y)
+        x,y=y,x+y
+
+
+x=fib()
+for i,j in zip(x,range (10)):
+    print(i)
+
+        
 
