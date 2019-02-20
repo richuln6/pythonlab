9275a92c0cf7b3c1b1e063d767e887c5fb419e31
richul <richuln6@gmail.com>
2019-02-20 11:26:48 +0530

    upgrade

--- problem2.py
+++ problem2.py
@@ -1,3 +1,15 @@
+def list_to_string(l):
+    s1=""
+    s4=""
+    for i in l:
+        s3="=".join(i[0]+i[1])
+        s4=s4+s3+";" 
+    return s4[:-1:]
+l=list_to_string([('a','b'),('c','d'),('e','f'),('g','h')])
+print(l)
+        
+
+
 def split_string(s):
     t=s.split(";")
     d={}
